f61392b8ec1492c74a29170f1598a7ac2190f0d3
RobertoFloris <florisroberto99@gmail.com>
2024-12-18 17:20:01 +0100

    fix show in router/posts.js

--- routers/posts.js
+++ routers/posts.js
@@ -1,47 +1,43 @@
-const posts = require("../data/posts")
+const posts = require("./../data/posts")
 
 const express = require('express');
 const router = express.Router();
 
 //Index
-//http://localhost:3000/posts
+//GET http://localhost:3000/posts
 router.get('/', (req,res) =>{
     // res.send ('Lista intera');
     res.json(posts)
 })
 
 //Show
-//http://localhost:3000/posts/Cracker alla barbabietola
+//GET http://localhost:3000/posts/Cracker alla barbabietola
 router.get('/:title', (req,res) =>{
-    // res.send (`Dettaglio elemento con titolo ${req.params.title}`);
-    const paramsTitle = req.params.title;
-    if(!paramsTitle){
-      return res.json(posts)
-    }
+    // res.send (`Dettaglio elemento con titolo ${req.params.title}`);const paramsTitle = req.params.title;
     const postByTitle = posts.find(post => post.title.includes(paramsTitle))
     res.json(postByTitle)
 })
 
 //Store
-//http://localhost:3000/posts
+//POST http://localhost:3000/posts
 router.post('/', (req,res) =>{
     res.send('Nuovo elemento')
 })
 
 //Update
-//http://localhost:3000/posts/Cracker alla barbabietola
+//PUT http://localhost:3000/posts/Cracker alla barbabietola
 router.put('/:title', (req,res)=>{
     res.send(`Modifico l'elemento con titolo ${req.params.title} `)
 })
 
 //Modify
-//http://localhost:3000/posts/Cracker alla barbabietola
+//PATCH http://localhost:3000/posts/Cracker alla barbabietola
 router.patch('/:title', (req, res)=>{
     res.send(`Modifico parzialmente l'elemento con titolo ${req.params.title}`)
 })
 
 //Destroy
-//http://localhost:3000/posts/Cracker alla barbabietola
+//DEL http://localhost:3000/posts/Cracker alla barbabietola
 router.delete('/:title', (req,res)=>{
     res.send(`Elimino elemento con titolo ${req.params.title}`)
 })
